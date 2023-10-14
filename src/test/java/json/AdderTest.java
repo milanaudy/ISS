bf8de05a6685f580b5f6b0d25484af3716c292d1
Martin Blazek <martinblazek6@gmail.com>
2023-10-14 16:40:06 +0200

    added CICD pipeline

---
+++ src/test/java/json/AdderTest.java
@@ -0,0 +1,13 @@
+package json;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdderTest {
+
+    @Test
+    void addCraft() {
+        assertEquals(2,2);
+    }
+}
